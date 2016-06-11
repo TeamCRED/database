2d32dc153406aeef141fa23ee4a80a3e04c39a00
CJ R <hello@cjr.co.de>
2016-06-10 22:05:58 -0600

    add award seeds

---
+++ seeds/awards.js
@@ -0,0 +1,35 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('awards').del().then(function () {
+    Promise.all([
+      knex('awards').insert({
+        'name': 'Drink an IPA',
+        'points': 50
+      }),
+      knex('awards').insert({
+        'name': 'Drink a Pilsner',
+        'points': 50
+      }),
+      knex('awards').insert({
+        'name': '1 of Every Beer',
+        'points': 500
+      }),
+      knex('awards').insert({
+        'name': 'Visit a Brewery',
+        'points': 400
+      }),
+      knex('awards').insert({
+        'name': 'Tour India',
+        'points': 700
+      }),
+      knex('awards').insert({
+        'name': 'Drink a Beer',
+        'points': 10
+      }),
+      knex('awards').insert({
+        'name': 'Drink a 6 Pack',
+        'points': 60
+      })
+    ]);
+  })
+};
--- seeds/batches.js
+++ seeds/batches.js
@@ -1,27 +1,28 @@
 
 exports.seed = function(knex, Promise) {
-  return Promise.all([
-    knex('batches').del(),
-    knex('batches').insert({
-      'date': '05-21-16',
-      'time': '12:22:00',
-      'quote': 'CARPE THA DIEM SEIZE THE CARP',
-      'tank': 'B4',
-      'beer_id': 'white-rascal'
-    }),
-    knex('batches').insert({
-      'date': '02-29-16',
-      'time': '10:13:00',
-      'quote': 'SHE TURNED ME INTO A NEWT!',
-      'tank': 'B4',
-      'beer_id': 'joe-s-pils'
-    }),
-    knex('batches').insert({
-      'date': '05-31-16',
-      'time': '15:29:00',
-      'quote': '',
-      'tank': 'FV32',
-      'beer_id': 'ipa'
-    }),
-  ]);
+  return knex('batches').del().then(function () {
+    return Promise.all([
+        knex('batches').insert({
+          'date': '05-21-16',
+          'time': '12:22:00',
+          'quote': 'CARPE THA DIEM SEIZE THE CARP',
+          'tank': 'B4',
+          'beer_id': 'white-rascal'
+        }),
+        knex('batches').insert({
+          'date': '02-29-16',
+          'time': '10:13:00',
+          'quote': 'SHE TURNED ME INTO A NEWT!',
+          'tank': 'B4',
+          'beer_id': 'joe-s-pils'
+        }),
+        knex('batches').insert({
+          'date': '05-31-16',
+          'time': '15:29:00',
+          'quote': '',
+          'tank': 'FV32',
+          'beer_id': 'ipa'
+        }),
+      ]);
+  });
 };

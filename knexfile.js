d9d6994f58a208ab848084b0cd28400946c205eb
dlinch <dlinch33@gmail.com>
2016-06-11 00:03:39 -0600

    added config for production database

--- knexfile.js
+++ knexfile.js
@@ -6,5 +6,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/teamcred'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
+
 };
